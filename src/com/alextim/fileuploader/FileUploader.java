bea9daafeff9320f27fb1454d4748016dc17fafc
Aleksey Timofeev on HOME-PC <alextim@list.com>
2012-07-14 16:01:30 +0300

    Initial commit for FileUploader project

---
+++ src/com/alextim/fileuploader/FileUploader.java
@@ -0,0 +1,17 @@
+package com.alextim.fileuploader;
+
+/**
+ * 
+ * Runnable that will upload films files from some directory
+ * 
+ *
+ */
+public class FileUploader implements Runnable{
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
